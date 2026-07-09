08da0c0893529c3080210cc13b6330741cd89aff
infectious112 <59289706+infectious112@users.noreply.github.com>
2026-07-09 21:15:02 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tally-journal-v1-2";
+const CACHE_NAME = "tally-journal-v1-2-1";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
